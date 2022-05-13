94907e7c207680880ea84b2b609e7049281b46f4
mariamaOlive <mariama.oliveira@yahoo.com.br>
2022-05-13 10:30:07 -0300

    Scrapy Americanas
    Co-authored-by: lucascouri2 <lucascouri2@users.noreply.github.com>
    Co-authored-by: Laianna <Laianna@users.noreply.github.com>

--- americanas/americanas/spiders/produtos.py
+++ americanas/americanas/spiders/produtos.py
@@ -2,6 +2,7 @@ from ast import IsNot
 import scrapy
 import pandas as pd
 from americanas.items import AmericanasItem
+import re
 
 class ProductSpider(scrapy.Spider):
 
@@ -13,16 +14,11 @@ class ProductSpider(scrapy.Spider):
 
 
     def start_requests(self):
+
         self.load_links()
 
-        urls = self.df_links["links"][:10].to_list()
-        # urls = ["https://www.americanas.com.br/produto/4875373969"]
-        
-        # urls = [
-        #         'http://en.wikipedia.org/wiki/Python_'
-        #         '%28programming_language%29',
-        #         'https://en.wikipedia.org/wiki/Functional_programming',
-        #         'https://en.wikipedia.org/wiki/Monty_Python']
+        urls = self.df_links["links"][:5].to_list()
+        #urls = ["https://www.americanas.com.br/produto/4875373969"]
         
         print(urls)
 
@@ -32,23 +28,23 @@ class ProductSpider(scrapy.Spider):
     def parse(self, response):
         
         items = AmericanasItem()
-
+       
         url = response.url
         title = response.xpath('//*[@id="rsyswpsdk"]/div/main/div[3]/div[1]/div/div[2]/h1/text()').extract()
-        ean = response.css('td:nth-child(2)::text').extract_first()
-        # ean = response.select('//*[@id="rsyswpsdk"]/div/main/div[8]/div[2]/div/div[2]/table').extract()
-        #descricao = response.xpath('//*[@id="info-section"]/div[2]/div/div/div/div').extract()
+        ean = re.findall(r'digo de barras</td><td class="spec-drawer__Text-sc-jcvy3q-5 fMwSYd">([0-9]+)|$', str(response.body))
         descricao = response.xpath('//*[@id="info-section"]/div[2]/div/div/div/div').xpath('normalize-space()').getall()
-        
-        print('URL is: {}'.format(url))
-        print('Title is: {}'.format(title))
-        print('EAN is: {}'.format(ean))
-        print('Descricao is: {}'.format(descricao))
-        
+      
+        '''print("###########################################################################")
+        print("URL is: {}".format(url))
+        print("Title is: {}".format(title))
+        print("EAN is: {}".format(ean))
+        print("Descricao is: {}".format(descricao))
+        print("###########################################################################")'''
+
         items['title'] = title[0] if len(title) != 0 else ""
-        items['ean'] = ean if ean is not None else ""
+        items['ean'] = ean[0]
+        #items['ean'] = ean if ean is not None else ""
         items['url'] = url
         items['descricao'] = descricao[0] if len(descricao) != 0 else ""
         
         yield items
-       
