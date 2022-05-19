0944eadd6a5e7742cf5fc7fa9ae66c2ef2dbecfc
Laianna <laiannalana@gmail.com>
2022-05-19 18:23:59 -0300

    Adicionando o scraper da Amazon com o scrapy

--- americanas/americanas/spiders/produtos_amazon.py
+++ americanas/americanas/spiders/produtos_amazon.py
@@ -12,20 +12,21 @@ class ProductAmazonSpider(scrapy.Spider):
     df_links  = None
     
     def load_links(self):
-        self.df_links = pd.read_csv('amazon_links_2.csv') #Mudar para os links corretos
+        self.df_links = pd.read_csv('../../../Dados/amazon_links_notebooks.csv')
 
 
     def start_requests(self):
 
-        # self.load_links()
+        self.load_links()
 
-        # urls = self.df_links["links"][500:].to_list()
-        urls = ["https://www.amazon.com.br/Notebook-Samsung-256GB-Windows-Chumbo/dp/B09N41J9K1/"]
+        urls = self.df_links["links"][:20].to_list()
+        #urls = ["https://www.amazon.com.br/Notebook-Samsung-256GB-Windows-Chumbo/dp/B09N41J9K1/"]
+        #urls = ["https://www.amazon.com.br/Mouse-Pad-Gaming-Havit-HV-MP830/dp/B0152HPXXE/"]
         
         print(urls)
 
-        # return [scrapy.Request(client.scrapyGet(url = url), callback = self.parse,  meta={'url': url}) for url in urls]
-        return [scrapy.Request(url = url, callback = self.parse, meta = {'url': url}) for url in urls]
+        return [scrapy.Request(client.scrapyGet(url = url), callback = self.parse,  meta={'url': url}) for url in urls]
+        #return [scrapy.Request(url = url, callback = self.parse, meta = {'url': url}) for url in urls]
 
 
     def parse(self, response):
@@ -34,15 +35,18 @@ class ProductAmazonSpider(scrapy.Spider):
         # print(str(response.body))
         url_meta = response.meta.get('url')
         url = response.url
-        titulo = response.xpath('//*[@id="productTitle"]/text()').extract()
         
-        tabela = response.xpath('//*[@id="productDetails_techSpec_section_1"]/tbody/tr[1]')
-        print(tabela)
-        ean = re.findall(r'EAN </th>   <td class="a-size-base prodDetAttrValue">\n                \u200e([0-9]+)|$', str(tabela))
-        #  EAN </th>   <td class="a-size-base prodDetAttrValue">\n                &lrm;7892509120784 </td>
-        descricao = response.xpath('//*[@id="productDescription"]/p/span').xpath('normalize-space()').getall()
-        preco = response.xpath('//*[@id="corePrice_feature_div"]/div/span/span[1]').extract()
-      
+        titulo = response.xpath('//*[@id="productTitle"]/text()').extract()
+        tabela = response.xpath('//*[@id="productDetails_techSpec_section_1"]/tr/*/text()').re('(\w+[^\n]+)')
+        tabela_string = ''.join(tabela) 
+        ean = re.findall(r'EAN.+?([0-9]+)|$', tabela_string)
+        descricao = response.xpath('//*[@id="productDescription"]/p/span/text()').getall()
+        preco = response.xpath('//*[@id="corePrice_feature_div"]/div/span/span[1]/text()').extract()
+
+        categoria = response.xpath('//a[@class="a-link-normal a-color-tertiary"]/text()').extract()
+        categoria = '/'.join(map(lambda x: x.strip(), categoria)).strip()
+
+        
         print("###########################################################################")
         print("URL: {}".format(url))
         print("Meta URL: {}".format(url_meta))
@@ -50,13 +54,15 @@ class ProductAmazonSpider(scrapy.Spider):
         print("EAN: {}".format(ean))
         print("Descricao: {}".format(descricao))
         print("Preco: {}".format(preco))
+        print("Categoria: {}".format(categoria))
         print("###########################################################################")
-
-        # items['titulo'] = titulo[0] if len(titulo) != 0 else ""
-        # items['ean'] = ean[0]
-        # #items['ean'] = ean if ean is not None else ""
-        # items['url'] = url_meta
-        # items['descricao'] = descricao[0] if len(descricao) != 0 else ""
-        # items['preco'] = descricao[0] if len(descricao) != 0 else ""
         
-        # yield items
+
+        items['titulo'] = titulo[0].strip() if len(titulo) != 0 else ""
+        items['ean'] = ean[0] if len(ean) != 0 else ""
+        items['url'] = url_meta
+        items['descricao'] = descricao[0] if len(descricao) != 0 else ""
+        items['preco'] = preco if preco is not None else ""
+        items['categoria'] = categoria if categoria is not None else ""
+
+        yield items
