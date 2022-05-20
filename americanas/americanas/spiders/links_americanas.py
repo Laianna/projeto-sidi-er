678e83da94d8dedcccbfa39c2c50d8c0997b0b1e
mariamaOlive <mariama.oliveira@yahoo.com.br>
2022-05-19 21:15:26 -0300

    Adicionado scraper de links
    Co-authored-by: Laianna <Laianna@users.noreply.github.com>

--- americanas/americanas/spiders/links_americanas.py
+++ americanas/americanas/spiders/links_americanas.py
@@ -1,73 +1,54 @@
 from ast import IsNot
 import scrapy
 import pandas as pd
-from americanas.items import AmericanasItem
+from americanas.items_links import LinksItem
 import re
 from scraper_api import ScraperAPIClient
-import Selector
-client = ScraperAPIClient('15158d4674ce528e593a775a4b90dcc9')
+# import Selector
+
+client = ScraperAPIClient('15158d4674ce528e593a775a4b90dcc9') #--> primeiro -> conta: Mari
+#client = ScraperAPIClient('39e2ae4a508557b0b178342d1664d3fd')
 
 class ProductAmazonSpider(scrapy.Spider):
 
     name = 'article'
-    df_links  = None
-    
-    def load_links(self):
 
-        for paginacao in range(1, 2):
+    def start_requests(self):
+
+        # self.load_links()
+        lista_links  = []
+        
+        for paginacao in range(1, 84):
             
             url = f"https://www.americanas.com.br/categoria/informatica/notebooks?viewMode=list&limit=24&offset={paginacao*24}"
             #url = f"https://www.americanas.com.br/categoria/celulares-e-smartphones/smartphone?viewMode=list&limit=24&offset={paginacao*24}"
 
-        self.df_links.append(url)
-
-
-    def start_requests(self):
-
-        self.load_links()
-
-        urls = self.df_links
+            lista_links.append(url)
+        
+        # urls = self.lista_links
         #urls = ["https://www.amazon.com.br/Notebook-Samsung-256GB-Windows-Chumbo/dp/B09N41J9K1/"]
         #urls = ["https://www.amazon.com.br/Mouse-Pad-Gaming-Havit-HV-MP830/dp/B0152HPXXE/"]
         
-        print(urls)
+        print(lista_links)
+        print(f"TAMANHO:>>>>>>>>>>>>{len(lista_links)}")
 
-        return [scrapy.Request(client.scrapyGet(url = url), callback = self.parse,  meta={'url': url}) for url in urls]
+        return [scrapy.Request(client.scrapyGet(url = url), callback = self.parse,  meta={'url': url}) for url in lista_links]
         # return [scrapy.Request(url = url, callback = self.parse, meta = {'url': url}) for url in urls]
 
 
     def parse(self, response):
-        sel = Selector(response)
+        print("URL: {}".format(response.meta.get('url')))
+        items = LinksItem()
         
-        # //*[@id="rsyswpsdk"]/div/main/div/div[3]/div[3]/div[1]/div
+        for i in range(1, 25):
+            #print(f"{i}")
+            for registro in response.selector.xpath(f'//*[@id="rsyswpsdk"]/div/main/div/div[3]/div[2]/div[{i}]/div/a/@href'): 
+                #print(registro.extract()) #.get()
+                
+                items['links'] = registro.extract() if registro.extract() is not None else ""
+                yield items
         
-        for entry in sel.xpath("//div[@class='src__Wrapper-sc-1wgxjb2-0 dUUAKQ']/a"):
-            print(entry.xpath('href').extract())
 
-        # lista_divs = resp.find_all("div", {"class": "src__Wrapper-sc-1wgxjb2-0 dUUAKQ"})
-
-        # for div in lista_divs:  
-        #     for link in div.findAll('a', attrs={'href': re.compile("^/")}):
-        #         links.append(f"https://www.americanas.com.br{link.get('href')}")
-        
-        
-        
-        
-        
-        # items = AmericanasItem()
-        # # print(str(response.body))
-        # url_meta = response.meta.get('url')
-        # url = response.url
-        
-        # titulo = response.xpath('//*[@id="productTitle"]/text()').extract()
-        # tabela = response.xpath('//*[@id="productDetails_techSpec_section_1"]/tr/*/text()').re('(\w+[^\n]+)')
-        # tabela_string = ''.join(tabela) 
-        # ean = re.findall(r'EAN.+?([0-9]+)|$', tabela_string)
-        # descricao = response.xpath('//*[@id="productDescription"]/p/span/text()').getall()
-        # preco = response.xpath('//*[@id="corePrice_feature_div"]/div/span/span[1]/text()').extract()
-
-        # categoria = response.xpath('//a[@class="a-link-normal a-color-tertiary"]/text()').extract()
-        # categoria = '/'.join(map(lambda x: x.strip(), categoria)).strip()
 
         
         '''print("###########################################################################")
@@ -81,11 +62,11 @@ class ProductAmazonSpider(scrapy.Spider):
         print("###########################################################################")'''
         
 
-        items['titulo'] = titulo[0].strip() if len(titulo) != 0 else ""
-        items['ean'] = ean[0] if len(ean) != 0 else ""
-        items['url'] = url_meta
-        items['descricao'] = descricao[0] if len(descricao) != 0 else ""
-        items['preco'] = preco if preco is not None else ""
-        items['categoria'] = categoria if categoria is not None else ""
+        # items['titulo'] = titulo[0].strip() if len(titulo) != 0 else ""
+        # items['ean'] = ean[0] if len(ean) != 0 else ""
+        # items['url'] = url_meta
+        # items['descricao'] = descricao[0] if len(descricao) != 0 else ""
+        # items['preco'] = preco if preco is not None else ""
+        # items['categoria'] = categoria if categoria is not None else ""
 
-        yield items
+        # yield items
