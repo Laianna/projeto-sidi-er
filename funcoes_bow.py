8c9d459739ddcd35c4256ce28b33109f126e06e0
Mariama Oliveira <mariama.oliveira@yahoo.com.br>
2022-07-13 15:55:49 -0300

    Testando o optuna com o bow
    Co-authored-by: Laianna <Laianna@users.noreply.github.com>

--- funcoes_bow.py
+++ funcoes_bow.py
@@ -74,8 +74,8 @@ def get_cooccurrence_bow_test_val(vectorizer_treino, X_):
 
         for chave, dic_indice in zip(cv_.vocabulary_.keys(), cv_.vocabulary_.values()):
             
-            if chave in vectorizer_treino.vocabulary_:
-                
+            if chave in vectorizer_treino.vocabulary_:             
+                # print(linha,"-",dic_indice,"-",chave)
                 co_vector[linha][dic_indice] = co_[linha][dic_indice]
         
     return co_vector
@@ -95,7 +95,7 @@ def get_all_bows(X_train, X_valid, X_test):
 def get_best_parameters(X_train_vec, X_valid_vec, y_train, y_valid):
     study = optuna.create_study(direction="maximize")
     obj_parcial = partial(objective, X_train_vec, X_valid_vec, y_train, y_valid)
-    study.optimize(obj_parcial, n_trials=20)
+    study.optimize(obj_parcial, n_trials=1)
 
     return study.best_params
 
@@ -107,11 +107,17 @@ def objective(X_train_vec, X_valid_vec, y_train, y_valid, trial):
                 "max_depth": trial.suggest_int("max_depth", 2, 128, log=True),
                 "criterion": trial.suggest_categorical('criterion', ["gini", "entropy"])}
 
-
     forest = RandomForestClassifier(**param_grid) 
     forest = forest.fit(X_train_vec, y_train)
     preds = forest.predict(X_valid_vec)
-    return preds
+ 
+    from sklearn.metrics import f1_score
+    metrica = f1_score(y_valid, preds, average=None)[0]
+
+    #from sklearn.metrics import accuracy_score
+    #metrica = accuracy_score(y_valid, preds)
+    
+    return metrica
 
 
 def pipeline_rf(name_dataset, X_train, y_train, X_valid, y_valid, X_test, y_test):
@@ -120,11 +126,11 @@ def pipeline_rf(name_dataset, X_train, y_train, X_valid, y_valid, X_test, y_test
     X_train_vec, X_valid_vec, X_test_vec = get_all_bows(X_train, X_valid, X_test)
 
     # #Get best parameters
-    #best_params = get_best_parameters(X_train_vec, X_valid_vec, y_train, y_valid)
+    best_params = get_best_parameters(X_train_vec, X_valid_vec, y_train, y_valid)
     
     #Train model with best parameters
-    #forest = RandomForestClassifier(**best_params) 
-    forest = RandomForestClassifier() 
+    forest = RandomForestClassifier(**best_params) 
+    #forest = RandomForestClassifier() 
     forest = forest.fit(X_train_vec, y_train)
 
     #Predict test dataset 
